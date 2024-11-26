de2153d8c95b6f2435e0daf2d9f2045504db9c02
João Paulo Leite Costa <joaopaulotds@gmail.com>
2024-11-26 15:44:29 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category(name) VALUES ('Books');
+INSERT INTO tb_category(name) VALUES ('Computer');
+INSERT INTO tb_category(name) VALUES ('Home');
+INSERT INTO tb_category(name) VALUES ('Pet');
